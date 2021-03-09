941c545ca79b49709b4d6da24917e0d468eef293
Jared Fernandez <jaredfern@cmu.edu>
2021-03-09 12:30:27 -0800

    Cleanup model/electra.py imports and style

--- model/electra.py
+++ model/electra.py
@@ -1,19 +1,19 @@
+import math
 from collections import defaultdict
 
-import math
 import torch
 from torch import nn
 
-from .layer import BertOnlyMLMHead
-from .model import UniterModel, UniterPreTrainedModel
 from .pretrain_vcr import UniterForPretrainingForVCR
+
 
 def _gelu_python(x):
     """
-    Original Implementation of the GELU activation function in Google BERT repo when initially created. For
-    information: OpenAI GPT's GELU is slightly different (and gives slightly different results): 0.5 * x * (1 +
-    torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3)))) This is now written in C in
-    torch.nn.functional Also see the Gaussian Error Linear Units paper: https://arxiv.org/abs/1606.08415
+    Original Implementation of the GELU activation function in Google BERT repo when initially
+    created. For information: OpenAI GPT's GELU is slightly different (and gives slightly
+    different results): 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 *
+    torch.pow(x, 3)))) This is now written in C in torch.nn.functional Also see the Gaussian
+    Error Linear Units paper: https://arxiv.org/abs/1606.08415
     """
     return x * 0.5 * (1.0 + torch.erf(x / math.sqrt(2.0)))
 
@@ -90,7 +90,8 @@ class UniterForElectraPretraining(nn.Module):
         super(UniterForElectraPretraining, self).__init__()
         self.generator = UniterForPretrainingForVCR.from_pretrained(
             config, ckpt, img_dim, img_label_dim)
-        self.discriminator = UniterDiscriminatorForElectraPretraining.from_pretrained(config, ckpt, img_dim, img_label_dim)
+        self.discriminator = UniterDiscriminatorForElectraPretraining.from_pretrained(
+            config, ckpt, img_dim, img_label_dim)
         self.gumbel_dist = torch.distributions.gumbel.Gumbel(0., 1.)
         self.electra_loss = ElectraLoss()
 
